3539454142493924dca7a6a3ecf6d16e62ee993c
Alexandru Dimian <alexandru.dimian@gmail.com>
2020-04-04 20:13:21 +0200

    points of interest!
    Former-commit-id: 24c6870780bde9331777eaccecdf9f52e2886133

--- js/ubahn-map.js
+++ js/ubahn-map.js
@@ -12,14 +12,34 @@ $(document).ready(function() {
 
 var height = isMobile ? window.devicePixelRatio * window.screen.height : screen.height;
 
-function getPointsOfInterest(data) {
-  var lat = data.position.lat;
-  var lon = data.position.lon;
+function getPointsOfInterest(station, promisedData) {
+  var wikiData;
+  var wikiTitle = '<h1>' + station.name + '</h1>';
+  var wikiCached = '<p class="cached">cached: ' + station.wiki_cache + '</p>';
+  var lat = station.position.lat;
+  var lon = station.position.lon;
   var aroundLocation = '(around:600,' + lat + ',' + lon + ');';
   var qu = 'https://lz4.overpass-api.de/api/interpreter?data=[out:json];(' +
       'node[~"tourism|leisure"~"museum|gallery|attraction"]' + aroundLocation +
       'way[~"tourism|leisure"~"museum|gallery|attraction"]' + aroundLocation +
     ')->._;out;'
+
+  if (station.wiki_cache !== false && station.wiki_cache !== undefined) {
+    wikiData = wikiTitle + promisedData + wikiCached
+  } else {
+    var resp = promisedData.query.pages[Object.keys(promisedData.query.pages)];
+    var wikiImage = resp.thumbnail.source
+    var wikiText = resp.extract
+    var wikiUrl = resp.fullurl
+
+    var formattedWikiText = wikiText
+      .split('<h2><span id="References">References</span></h2>')[0]
+      .split('<h2><span id="Gallery">Gallery</span></h2>')[0]
+
+    wikiData = wikiTitle +
+      '<img src=' + '"' + wikiImage + '">' + formattedWikiText +
+      '<a href="' + wikiUrl + '">' + wikiUrl + '</a>'
+  }
 
   $.ajax({
     url: qu,
@@ -46,13 +66,14 @@ function getPointsOfInterest(data) {
       });
 
       var pointsOfInterest = '';
+
       $.each(formattedData, function(_, poi) {
         $.each(poi, function(_, poiValue) {
           pointsOfInterest += '<p>' + poiValue + '</p>'
         })
       })
 
-      return pointsOfInterest
+      showSidebar(wikiData + pointsOfInterest)
     }
   });
 };
@@ -86,53 +107,37 @@ function formatAddress(e) {
 }
 
 function getWikiData(station) {
-  var wikiStation = station.name.replace(" ", "_").concat("_(Berlin_U-Bahn)");
-  var wikiTitle = '<h1>' + station.name + '</h1>';
-  var wikiCached = '<p class="cached">cached: ' + station.wiki_cache + '</p>';
-
   if (station.wiki_cache !== false && station.wiki_cache !== undefined) {
-    $.ajax({
-      url: 'articles/' + station.name + '.html',
-      success: function(data) {
-        console.log(data)
-        $("#sidebar").show();
-        $("#sidebar-buttons").show();
-        $('#sidebar-content').html(wikiTitle + data + wikiCached)
-      }
-    });
+    $.when(
+      $.ajax(
+        { url: 'articles/' + station.name + '.html' })
+    )
+      .then(
+      function(data) {
+        getPointsOfInterest(station, data) }
+    )
   } else {
-    $.ajax({
-      url: 'https://en.wikipedia.org/w/api.php',
-      data: {
-        action: 'query',
-        titles: wikiStation,
-        redirects: 1,
-        pithumbsize: 600,
-        exsectionformat: "raw",
-        prop: 'extracts|pageimages|info',
-        inprop: 'url',
-        format: 'json'
-      },
-      dataType: 'jsonp',
-      success: function(data) {
-        var resp = data.query.pages[Object.keys(data.query.pages)];
-        var wikiImage = resp.thumbnail.source
-        var wikiText = resp.extract
-        var wikiUrl = resp.fullurl
-
-        var formattedWikiText = wikiText
-          .split('<h2><span id="References">References</span></h2>')[0]
-          .split('<h2><span id="Gallery">Gallery</span></h2>')[0]
-
-        var wikiData = wikiTitle +
-          '<img src=' + '"' + wikiImage + '">' + formattedWikiText +
-          '<a href="' + wikiUrl + '">' + wikiUrl + '</a>'
-
-        $("#sidebar").show();
-        $("#sidebar-buttons").show();
-        $('#sidebar-content').html(wikiData)
+    $.when(
+      $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php',
+        data: {
+          action: 'query',
+          titles: station.name.replace(" ", "_").concat("_(Berlin_U-Bahn)"),
+          redirects: 1,
+          pithumbsize: 600,
+          exsectionformat: "raw",
+          prop: 'extracts|pageimages|info',
+          inprop: 'url',
+          format: 'json'
+        },
+        dataType: 'jsonp'
+      })
+    ).then(
+      function(x) {
+        console.log(x)
+        getPointsOfInterest(station, x);
       }
-    });
+    )
   }
 }
 
@@ -169,3 +174,10 @@ d3.json('./json/berlin-ubahn.json').then(function(data) {
     svg.select('g').attr('transform', d3.event.transform.toString());
   }
 });
+
+function showSidebar(sidebarHtml) {
+  $("#sidebar").show();
+  $("#sidebar-buttons").show();
+  $('#sidebar-content').html(sidebarHtml)
+}
+
