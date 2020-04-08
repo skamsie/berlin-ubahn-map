a66e0b945a070c7e8e9983da62c80a81af54bdc5
Alexandru Dimian <alexandru.dimian@gmail.com>
2020-04-08 11:41:05 +0200

    added Vinetastrasse, Pankow, Schoenhauser_Allee, Eberswalder_Strasse and Weberwiese
    Former-commit-id: b5143fecd6585a18162287945f0fb9a4c01a8591

--- js/ubahn-map.js
+++ js/ubahn-map.js
@@ -81,24 +81,6 @@ function getWikiData(station) {
           '<img src=' + '"' + wikiImage + '">' + formattedWikiText +
           '<a href="' + wikiUrl + '">' + wikiUrl + '</a>'
 
-        function saveData(data, fileName) {
-            var a = document.createElement("a");
-            document.body.appendChild(a);
-            a.style = "display: none";
-
-            var json = JSON.stringify(data),
-                blob = new Blob([data], {type: "text/html;charset=utf-8"}),
-                url = window.URL.createObjectURL(blob);
-            a.href = url;
-            a.download = fileName;
-            a.click();
-            window.URL.revokeObjectURL(url);
-        }
-        saveData(
-          '<img src=' + '"' + wikiImage + '">' + formattedWikiText +
-          '<a href="' + wikiUrl + '">' + wikiUrl + '</a>', station.name + '.html'
-        )
-
         showSidebar(wikiData)
       }
     });
